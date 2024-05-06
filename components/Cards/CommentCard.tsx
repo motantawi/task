63a2780730606cc31896f2aae998939f57ae9255
motantawi <mostafa50.hh@gmail.com>
2024-05-06 15:15:47 +0300

    fix: design

--- components/Cards/CommentCard.tsx
+++ components/Cards/CommentCard.tsx
@@ -8,17 +8,27 @@ const CommentCard = ({ body }: Comment) => {
       <div className="flex justify-between items-center mb-5">
         <div className="flex items-center gap-2">
           <div className="rounded-full bg-gray-400 w-10 h-10"></div>
-          <div className="text-lg font-medium text-gray-800">Youssef Helmy</div>
-          <div className="text-gray-500">2 weaks ago</div>
+          <div className="flex flex-col sm:flex-row sm:items-center sm:gap-2">
+            <div className="text-sm sm:text-base md:text-lg font-medium text-gray-800">
+              Youssef Helmy
+            </div>
+            <div className="text-xs md:text-sm text-gray-500">2 weaks ago</div>
+          </div>
         </div>
-        <button
-          className="flex items-center gap-2 text-[#5357B6] font-semibold"
-        >
-          <Image src={"/assets/Path.svg"} alt="share" width={20} height={20} />
+        <button className="flex items-center gap-2 text-[#5357B6] font-semibold">
+          <Image
+            src={"/assets/Path.svg"}
+            alt="share"
+            width={20}
+            height={20}
+            className="w-4"
+          />
           Reply
         </button>
       </div>
-      <p className="text-gray-600 leading-relaxed mb-6">{body}</p>
+      <p className="text-xs sm:text-sm text-gray-500 leading-relaxed mb-6">
+        {body}
+      </p>
     </div>
   );
 };
